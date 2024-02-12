6ad18e6f93c91f3fa7cf5bd126931c6c3a42d8e8
Sofia Panuganti <sofiapanuganti@gmail.com>
2024-02-12 16:00:56 -0600

    included projects section with information filled out

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -3,42 +3,41 @@ import Project from "./Project";
 import { Element } from 'react-scroll';
 import nodejs from './images/nodejsLogo.png';
 import react from './images/reactLogo.png';
-import html from './images/htmlLogo.png';
-import css from './images/cssLogo.png';
+// import html from './images/htmlLogo.png';
+// import css from './images/cssLogo.png';
 import javascript from './images/javascriptLogo.png';
-import java from './images/javaLogo.png';
+// import java from './images/javaLogo.png';
 import express from './images/express-js.png';
-import git from './images/gitLogo.png';
+// import git from './images/gitLogo.png';
+import mongodb from './images/mongodbLogo.png';
 import googlemaps from './images/googleMapsLogo.png';
-
+import auth0 from './images/auth0Logo1.png';
 
 
 function Projects() {
 
   const projects = [
+        {
+      title: "Virtual Garden",
+      imageSrc: "/img/cryptocrowd2.png",
+      description: "I made this Virtual Garden web application incorporating the MERN stack. The web application has Auth0 authentication on both the frontend and backend. Users can create gardens and add plants to the gardens - to virtually track and manage their home gardens. I styled the frontend using TailwindCSS. Work in progress.",
+      techStack: [<img src = {mongodb} alt="MongoDB" className="project-logo"/>, <img src = {express} alt="Express.js" className="project-logo"/>, <img src = {nodejs} alt="NodeJS" className="project-logo"/>, <img src = {react} alt="ReactJS" className="project-logo"/>, <img src = {auth0} alt="Auth0" className="project-logo mt-1.5"></img> ],
+      codeLink: "https://github.com/sofiap14/virtual-garden",
+    },
     {
       title: "Weather App",
       imageSrc: "/weather1.png",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nulla nec metus interdum euismod. Sed id sapien id arcu eleifend egestas.",
-      techStack: [<img src = {nodejs} alt="Node.js" className="project-logo"/>, <img src = {express} alt="Express.js" className="project-logo"/>, <img src = {googlemaps} alt="Google Maps API" className="project-logo"/>, <img src = {javascript} alt="JavaScript" className="project-logo"/>],
-      demoLink: "https://www.example.com/demo1",
-      codeLink: "https://www.example.com/code1",
-      liveLink: "https://www.example.com/live1",
+      description: "Weather web application that fetches real-time weather data from OpenWeatherMap API, displaying temperature and humidity information for any city that the user inputs. The app also integrates the Google Maps API for location visualization. Built using Node.js and Express.js, with a CSS frontend.",
+      techStack: [<img src = {nodejs} alt="Node.js" className="project-logo"/>, <img src = {express} alt="Express.js" className="project-logo"/>, <img src = {googlemaps} alt="Google Maps API" className="project-logo"/>, <img src = {javascript} alt="JavaScript" className="project-logo"/> ],
+      codeLink: "https://github.com/sofiap14/node-weather-app",
     },
     {
       title: "Dictionary App",
       imageSrc: "/dictionary2.png",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nulla nec metus interdum euismod. Sed id sapien id arcu eleifend egestas.",
-      techStack: ["Tech 1", "Tech 2", "Tech 3"],
-      demoLink: "https://www.example.com/demo2",
-      codeLink: "https://www.example.com/code2",
-    },
-    {
-      title: "Cryptocurrency Analysis",
-      imageSrc: "/img/cryptocrowd2.png",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae nulla nec metus interdum euismod. Sed id sapien id arcu eleifend egestas.",
-      techStack: ["Tech 1", "Tech 2", "Tech 3"],
-      codeLink: "https://www.example.com/code3",
+      description: "Dictionary web application that uses Merriam-Webster Dictionary API for the user to explore the meaning of any word they enter. Built using Node.js and Express.js with a clean CSS frontend.",
+      techStack: [<img src = {nodejs} alt="Node.js" className="project-logo"/>, <img src = {express} alt="Express.js" className="project-logo"/>],
+      demoLink: "https://youtu.be/eSuvpIrVjHE",
+      codeLink: "https://github.com/sofiap14/dictionary-webapp",
     },
   ];
 
