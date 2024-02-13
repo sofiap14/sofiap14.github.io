5309665d50d8fe56e3cf4affe82d202e5d26b65a
Sofia Panuganti <sofiapanuganti@gmail.com>
2024-02-13 17:46:23 -0600

    navbar updated to be responsive with xs

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -17,9 +17,9 @@ export default function NavBar() {
       if (offset < projectsSection) {
         setActiveSection("about")
       }
-      else if (offset < skillsSection) {
+      else if (offset < skillsSection - 200) {
         setActiveSection("projects");
-      } else if (offset < educationSection) {
+      } else if (offset < educationSection - 200) {
         setActiveSection("skills");
       } else if (offset < contactSection - 290) {
         setActiveSection("education");
@@ -48,7 +48,7 @@ export default function NavBar() {
 
   return (
     <div className="h-[4.5rem] w-full sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full text-gray-300 text-sm">
-      <nav className="sm: text-xs sm:top-6 fixed top-0 left-1/2 transform -translate-x-1/2 p-5 mt-3 h-10 flex justify-center items-center bg-opacity-80 border border-pastel-purpledeep rounded-full border-opacity-40 shadow-lg shadow-black/[0.01] backdrop-blur-[0.5rem] dark:bg-pastel-purpledeep dark:bg-opacity-75">
+      <nav className="text-xs sm:top-6 fixed top-0 left-1/2 transform -translate-x-1/2 p-5 mt-3 h-10 flex justify-center items-center bg-opacity-80 border border-pastel-purpledeep rounded-full border-opacity-40 shadow-lg shadow-black/[0.01] backdrop-blur-[0.5rem] dark:bg-pastel-purpledeep dark:bg-opacity-75">
         <ul className="flex space-x-6 md:space-x-10 lg:space-x-14 ">
           <li className={`inline-block ${activeSection === "about" ? "active-section" : ""}`}>
             <button className="hover:text-pastel-purple" onClick={() => scrollToSection("about")}>Home</button>
